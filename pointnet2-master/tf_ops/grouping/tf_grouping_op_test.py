38359bb2ea873090de4dcb84b35636a2c7767f28
ItsShriks <nakhyeshrikar@icloud.com>
2025-04-12 14:37:05 +0200

    Handed over to NVIDIA

--- pointnet2-master/tf_ops/grouping/tf_grouping_op_test.py
+++ pointnet2-master/tf_ops/grouping/tf_grouping_op_test.py
@@ -3,26 +3,25 @@ import numpy as np
 from tf_grouping import query_ball_point, group_point
 
 class GroupPointTest(tf.test.TestCase):
-  def test(self):
-    pass
+    def test_grad(self):
+        tf.compat.v1.disable_eager_execution()  # Ensure TF1 behavior for gradient tests
+        with tf.device('/gpu:0'):
+            points = tf.constant(np.random.random((1, 128, 16)).astype('float32'))
+            xyz1 = tf.constant(np.random.random((1, 128, 3)).astype('float32'))
+            xyz2 = tf.constant(np.random.random((1, 8, 3)).astype('float32'))
+            radius = 0.3
+            nsample = 32
+            idx, pts_cnt = query_ball_point(radius, nsample, xyz1, xyz2)
+            grouped_points = group_point(points, idx)
 
-  def test_grad(self):
-    with tf.device('/gpu:0'):
-      points = tf.constant(np.random.random((1,128,16)).astype('float32'))
-      print points
-      xyz1 = tf.constant(np.random.random((1,128,3)).astype('float32'))
-      xyz2 = tf.constant(np.random.random((1,8,3)).astype('float32'))
-      radius = 0.3 
-      nsample = 32
-      idx, pts_cnt = query_ball_point(radius, nsample, xyz1, xyz2)
-      grouped_points = group_point(points, idx)
-      print grouped_points
+        with self.session():
+            print("---- Going to compute gradient error")
+            err = tf.test.compute_gradient_error(
+                points, (1, 128, 16),
+                grouped_points, (1, 8, 32, 16)
+            )
+            print("Gradient error:", err)
+            self.assertLess(err, 1e-4)
 
-    with self.test_session():
-      print "---- Going to compute gradient error"
-      err = tf.test.compute_gradient_error(points, (1,128,16), grouped_points, (1,8,32,16))
-      print err
-      self.assertLess(err, 1e-4) 
-
-if __name__=='__main__':
-  tf.test.main() 
+if __name__ == '__main__':
+    tf.test.main()
