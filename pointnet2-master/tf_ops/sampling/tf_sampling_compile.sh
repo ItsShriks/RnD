b62306705da72df070f8103709a42dcda31b300b
Shrikar Nakhye <126857057+ItsShriks@users.noreply.github.com>
2025-04-15 13:43:42 +0200

    Update tf_sampling_compile.sh

--- pointnet2-master/tf_ops/sampling/tf_sampling_compile.sh
+++ pointnet2-master/tf_ops/sampling/tf_sampling_compile.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# CUDA 11.8
-/usr/local/cuda-11.8/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+# Define CUDA path (based on your env)
+CUDA_PATH=/home/jovyan/.local/opt/conda/envs/itshriks
 
-# Get the TensorFlow include path for Python 3.x
-TF_INCLUDE_PATH=$(python3 -c "import tensorflow as tf; print(tf.sysconfig.get_include())")
+# Compile the CUDA code
+$CUDA_PATH/bin/nvcc tf_sampling.cu -o tf_sampling.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
-# TF1.2
-g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I $TF_INCLUDE_PATH -I /usr/local/cuda-11.8/include -lcudart -L /usr/local/cuda-11.8/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
-
-# TF1.4 (optional, if you need to support TF1.4)
-#g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I $TF_INCLUDE_PATH -I /usr/local/cuda-11.8/include -I /usr/local/lib/python3.8/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-11.8/lib64/ -L/usr/local/lib/python3.8/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+# Compile the shared object using g++
+g++ -std=c++11 tf_sampling.cpp tf_sampling.cu.o -o tf_sampling_so.so -shared -fPIC \
+    -I$CUDA_PATH/include \
+    -I$CUDA_PATH/lib/python3.*/site-packages/tensorflow/include \
+    -L$CUDA_PATH/lib \
+    -L$CUDA_PATH/lib64 \
+    -lcudart
